ee7f1e89aa7c2e9b855d93c7b0c36669363f730c
allengoh <allen1311@hotmail.com>
2022-07-17 01:55:17 +0100

    Ignore previous commit: Added a method to save data to a csv file

--- directory.rb
+++ directory.rb
@@ -30,6 +30,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -41,7 +42,7 @@ end
 
 def save_students
   #open the file for writing
-  file = File.open("student.csv", "w")
+  file = File.open("students.csv", "w")
   #iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
@@ -59,6 +60,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -81,16 +84,5 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
-def save_students
-  #open the file for writing
-  file = File.open("student.csv", "w")
-  #iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
 
 interactive_menu
